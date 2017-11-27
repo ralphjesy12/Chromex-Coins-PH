8c54e6e5b364761b353865ceae032a80680efed8
Ralph John Galindo <ralphjesy@gmail.com>
2017-11-27 09:24:30 +0800

    Adding Notifications for  Lowest/Highest Today Price

--- background.js
+++ background.js
@@ -1,10 +1,14 @@
 var last = 0;
-
+var lowest = Number.MAX_SAFE_INTEGER;
+var highest = 0;
+var lastday = (new Date()).getDate();
+var id = 0;
 document.addEventListener('DOMContentLoaded', function () {
     refreshPrice();
 });
 
 function refreshPrice(){
+
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "https://quote.coins.ph/v1/markets", true);
     xhr.onreadystatechange = function() {
@@ -13,6 +17,7 @@ function refreshPrice(){
                 var resp = JSON.parse(xhr.responseText);
 
                 price = resp.markets[4].ask;
+                priceSell = resp.markets[4].bid;
                 text = (Math.round(price/1000));
                 if(last > price){
                     chrome.browserAction.setBadgeBackgroundColor({
@@ -39,6 +44,47 @@ function refreshPrice(){
                 chrome.browserAction.setTitle({
                     title : 'PHP ' + price
                 });
+
+                // notifications
+
+
+                if(lastday != (new Date()).getDate()){
+
+                    lastday = (new Date()).getDate();
+                    lowest = Number.MAX_SAFE_INTEGER;
+                    highest = 0;
+                }
+
+                if(price < lowest){
+                    lowest = price;
+
+                    console.log("Lowest Today : " + "\n" + "Buy:" + price + "\n" + "Sell: " + priceSell);
+                    id++;
+                    chrome.notifications.create(
+                        'id' + id ,{
+                            type:"basic",
+                            title:"BTC Price Update",
+                            message:"Lowest Today : " + "\n" + "Buy:" + price + "\n" + "Sell: " + priceSell,
+                            iconUrl:"images/coins-48.png"
+                        },
+                    );
+
+                }
+
+                if(price > highest){
+                    highest = price;
+
+                    console.log("Highest Today : " + "\n" + "Buy:" + price + "\n" + "Sell: " + priceSell);
+                    id++;
+                    chrome.notifications.create(
+                        'id' + id ,{
+                            type:"basic",
+                            title:"BTC Price Update",
+                            message:"Highest Today : " + "\n" + "Buy:" + price + "\n" + "Sell: " + priceSell,
+                            iconUrl:"images/coins-48.png"
+                        },
+                    );
+                }
 
                 setTimeout(function(){
                     refreshPrice();
